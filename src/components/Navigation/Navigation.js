3002f4afe7d17246f476715b04d876faf2949cb2
First Last <salmanashraf853@gmail.com>
2019-07-07 15:20:40 +0100

    smart brain

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+      return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+        </nav>
+      );
+}
+
+export default Navigation;
